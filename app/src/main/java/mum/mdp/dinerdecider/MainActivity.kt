2dcf64cf4f2a15e599c4384a5c8ec1ca12c92d37
KhaliunSam <50113961+KhaliunSam@users.noreply.github.com>
2020-09-02 20:47:51 -0500

    Lesson 3

---
+++ app/src/main/java/mum/mdp/dinerdecider/MainActivity.kt
@@ -0,0 +1,24 @@
+package mum.mdp.dinerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val foods = arrayListOf<String>()
+        foods.addAll(listOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
+        decideBtn.setOnClickListener {
+            food.text = "${foods.random()}"
+
+        }
+        val food = addFoodBtn.setOnClickListener { v ->
+            foods.add("${addFood.text}")
+        }
+
+    }
+}
